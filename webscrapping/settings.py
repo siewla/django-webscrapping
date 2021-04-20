2ead293e79ebb40e8ff5bce6c6d319fccc479e89
siewla <siewlapang@gmail.com>
2021-04-21 03:52:49 +0800

    remove django-heroku

--- webscrapping/settings.py
+++ webscrapping/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -140,5 +139,3 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
